9941940abc0a6ab9603cb34b5a41c8de76620637
varunkumarnr <varunnrk@gmail.com>
2020-08-19 20:25:57 +0530

    ui cart

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import '../styles/cartpage.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import addtocart from '../actions/CartActions';
 
 
 function CartPage(props) {
+    const cart = useSelector(state => state.cart);
+
+    const { cartItems } = cart;
     const productId = props.match.params.id;
     const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
     //console.log(qty);
@@ -21,10 +24,28 @@ function CartPage(props) {
             <div className = "cart title">
                      Shopping Cart
             </div>
-            <div className="">
-                  
-            </div>
-                     
+            <ul className = "cart-products"> 
+              <li className="cart-items-title"> 
+                   <h3 className= "cart-item-products-title">products</h3> 
+                   <h3 className=" cart-items-price-title">price</h3> 
+              </li>
+              {
+          cartItems.length === 0 ?
+            <div>
+              Cart is empty
+          </div>
+            :
+            cartItems.map(item => 
+                <li className=" cart-container">
+                      <img className = "cart-item-image" src = {item.image} ></img>
+                      <div className="cart-item-desc">
+                      <h4 className="cart-item-name"> {item.name } </h4>
+                      </div>
+                </li>
+            )
+            }
+            </ul>
+            
             </div>
             <div className="pay-section">
                 pay-section
--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,3 +1,24 @@
-function addtoCartReducer(state = {}){
+const { CART_ADD_ITEM } = require("../constants/productconstants");
+
+function cartReducer(state = { cartItems: [] }, action) {
+    switch (action.type) {
+      case CART_ADD_ITEM:
+        const item = action.payload;
+        const product = state.cartItems.find(x => x.product === item.product);
+        if (product) {
+          return {
+            cartItems:
+              state.cartItems.map(x => x.product === product.product ? item : x)
+          };
+        }
+        return { cartItems: [...state.cartItems, item] };
+        default : 
+            return state
+        
+   }
+   
+      
    
 }
+
+export { cartReducer }
